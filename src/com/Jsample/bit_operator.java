c957019291f0dfd8b1994721c4d147cfd06a018e
Free-Geter <1321687936@qq.com>
2021-03-17 19:40:55 +0800

    Initial commit

---
+++ src/com/Jsample/bit_operator.java
@@ -0,0 +1,22 @@
+package com.Jsample;
+
+public class bit_operator {
+    public static void main(String[] args){
+        int i = -3;
+        // 正数右移，空位补0，负数补1
+        System.out.println(i<<2);
+        System.out.println(i>>2);
+        // 无符号右移
+        System.out.println(i>>>2);
+
+        // 对二进制位按位进行与、或、异或运算
+        System.out.println(12|5);
+        System.out.println(12&5);
+        System.out.println(12^5);
+
+        // 计算反码
+        System.out.println(~12);
+
+        
+    }
+}
