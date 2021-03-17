cb6d63308270354d7cb4ec1763f53268e745fc62
Free-Geter <1321687936@qq.com>
2021-03-18 07:25:35 +0800

    2021。03。17

---
+++ src/com/Jsample/array/Demo_0.java
@@ -0,0 +1,17 @@
+package com.Jsample.array;
+
+public class Demo_0 {
+    public static void main(String[] args) {
+        // 静态初始化
+        int[] numbers = {1,2,3,4,5};
+
+        // 动态初始化
+        double[] temp = new double[10];
+
+        // for-each 循环遍历数组
+        for (var x :
+                numbers) {
+            System.out.println(x);
+        }
+    }
+}
