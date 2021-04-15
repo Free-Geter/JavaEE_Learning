ea323d2eabcfd40511ad92341cdc0296c1f240e0
Free-Geter <1321687936@qq.com>
2021-04-15 22:12:45 +0800

    04 15 10 4 HashSet

--- src/com/lesson6/Exception.java
+++ src/com/lesson6/Exception.java
@@ -1,7 +1,6 @@
 package com.lesson6;
 
-import javax.crypto.ExemptionMechanism;
-import javax.crypto.ExemptionMechanismException;
+import java.util.Arrays;
 
 public class Exception {
 
@@ -9,20 +8,54 @@ public class Exception {
     static Exception e = null;
     private int h = 0;
 
-    public static void main(String[] args) {
+    /**
+     * 在try 代码块中的代码，如果出现了异常，那么后面的就不会继续执行了，所以一般只能捕获到一个异常
+     * 因为后面的异常代码都不会被执行
+     * 子类重写父类的方法时，子类不能抛出比父类更大范围的异常
+     */
+    // 如果main方法抛出异常，将会抛给JVM，在程序中不能处理
+    public static void main(String[] args) throws java.lang.Exception {
         try {
-            System.out.println(test[2]);
-            System.out.println(e.h);
-            System.out.println(1/e.h);
-        }catch (ArrayIndexOutOfBoundsException e){
-            System.out.printf("数组越界");
+
+            A a = new A(150);
+            System.out.println(a.age);
+
+        }catch (ArrayIndexOutOfBoundsException e) {
+            // 获取异常信息
+            System.out.println(e.getMessage());
+            // 获取异常位置
+            e.printStackTrace();
+            System.out.print("数组越界");
         }catch (NullPointerException e1){
-            System.out.printf("空指针异常");
+            System.out.print("空指针异常");
         }catch (ArithmeticException e2){
-            System.out.printf("数学运算错误");
+            System.out.print("数学运算错误");
+        } catch (java.lang.Exception e3){
+            e3.printStackTrace();
         }finally {
-            System.out.printf("=========");
+            System.out.print("=========");
         }
-        System.out.printf("程序执行完毕");
+        System.out.print("程序执行完毕");
+    }
+}
+
+class B {
+    int i = 0;
+    void test() throws NullPointerException {
+        B b = null;
+        System.out.println(b.i);
+    }
+}
+
+class A extends B{
+    int age;
+    // 禁止抛出Exception异常，这个异常比父类的异常的范围更大
+//    void test() throws java.lang.Exception {}
+    A (int age) throws java.lang.Exception {
+        if (age > 0 && age < 130)
+            this.age = age;
+        else {
+            throw new java.lang.Exception("年龄不在0 - 130之间");
+        }
     }
 }
